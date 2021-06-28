6e13baeadaf89164cacae4b2b1acf74d1ef3df43
shuzaei <shuzaei@gmail.com>
2021-06-29 02:25:32 +0900

    Fix test

--- tests/na/interger-input.sh
+++ tests/na/interger-input.sh
@@ -3,9 +3,11 @@
 id="na/integer-input"
 in=(
     "abcdefgh12345678ABCDEFGH\n"
+    "1000000000000000\n"
 )
 out=(
     "0000000012345678\n"
+    "1000000000000000\n"
 )
 
 root="$(dirname "$0")/../.."
@@ -13,9 +15,17 @@ root="$(dirname "$0")/../.."
 bcc "$root/$id.bc" "$root/$id"
 flag=true
 for ((i = 0; i < ${#in[@]}; i++)) {
-    diff <("$root/$id" < <(printf "${in[i]}")) <(printf "${out[i]}") \
-    && printf "[test %d/%d] \e[92mOK\e[0m\n" $(($i+1)) ${#in[@]}\
-    || printf "[test %d/%d] \e[91mFAILED\e[0m\n" $(($i+1)) ${#in[@]} && flag=false
+    diff <("$root/$id" < <(printf "${in[i]}")) <(printf "${out[i]}")
+    if [ $? = 0 ]; then
+        printf "[test %d/%d] \e[92mOK\e[0m\n" $(($i+1)) ${#in[@]}
+    else
+        printf "[test %d/%d] \e[91mFAILED\e[0m\n" $(($i+1)) ${#in[@]}
+        flag=false
+    fi
 }
-[[ $flag ]] && printf "\e[92mOK\e[0m\n" || printf "\e[91mFAILED\e[0m\n"
-[[ $flag ]]
+if [ $flag = true ]; then
+    printf "\e[92mOK\e[0m\n"
+else
+    printf "\e[91mFAILED\e[0m\n"
+fi
+[[ $flag == true ]]
